8ff98cc2b094e33592989c854ba7a5157510e30b
Bayu <rizqi.bayu@ui.ac.id>
2024-05-04 17:16:36 +0700

    Experiment 1.2: Understanding how it works.

--- src/main.rs
+++ src/main.rs
@@ -107,6 +107,7 @@ fn main() {
         println!("Bayu's Computer: done!");
     });
 
+    println!("Bayu's Computer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
